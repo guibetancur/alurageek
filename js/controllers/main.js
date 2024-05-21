98f2dd5aa4e3238cd5aea83243407935c4db2e30
Guillermo Betancur <guibetancur@gmail.com>
2024-05-21 11:35:08 -0500

    Alura Geek

---
+++ js/controllers/main.js
@@ -0,0 +1,90 @@
+import { servicesProducts } from "../services/product-services.js"
+
+const productContainer = document.querySelector("[data-product]")
+const form = document.querySelector("[data-form]")
+// var eraser = []
+
+function createCard(name, price, image, id) {
+  const card = document.createElement("div")
+  card.classList.add("card")
+
+  card.innerHTML = `
+          <div class="img-container">
+            <img src="${image}" alt="${name}">
+          </div>
+
+          <div class="card-container--info">
+            <p>${name}</p>
+            <div class="card-container--value">
+              <p>$ ${price}</p>
+              <button class="delete-button" data-id="${id}" onclick="deleteProduct('${id}')">
+                <img src="./assets/trashIcon.svg" alt="Eliminar">
+              </button>
+            </div>
+          </div>
+          `
+  productContainer.appendChild(card)
+  return card
+}
+
+const eraser = document.querySelectorAll("[data-id]")
+eraser.forEach(btn => {
+  const id = btn.getAttribute("id")
+  console.log("id del botón:", id)
+  btn.addEventListener('click', () => deleteProduct())
+})
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+async function deleteCard(id) {
+  await conexionAPI.deleteProduct(id);
+  window.location.href = "../index.html";
+}
+
+window.addEventListener('load', function () {
+  const btnDelete = document.querySelectorAll("[data-delete]");
+  const html = document.documentElement.outerHTML;
+
+  btnDelete.forEach(btn => {
+    const id = btn.getAttribute("id");
+    console.log("id del boton " + id);
+    btn.addEventListener('click', () => deleteCard(id));
+  });
+});
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+const render = async () => {
+  try {
+    const listProducts = await servicesProducts.productList()
+    
+    listProducts.forEach(product => {
+      productContainer.appendChild(
+        createCard(product.name, product.price, product.image, product.id)
+      );
+    });
+  } catch (error) {
+    // console.log("revisar aca");
+    console.log(error);
+  } 
+  // finally {
+    // debugger
+    // eraser = document.querySelectorAll("[data-id]")
+    // eraser = document.getElementsByClassName("delete-button")
+    // console.log(eraser);
+    // eraser.data.addEventListener("click", (event) => {
+    //   event.preventDefault()
+    //   servicesProducts.deleteProduct(id)
+    // })
+  // }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const name = document.querySelector("[data-name]").value
+  const price = document.querySelector("[data-price]").value
+  const image = document.querySelector("[data-image]").value
+
+  servicesProducts.createProduct(name, price, image).then(res => console.log(res)).catch(err => console.log(err));
+})
+
+
+render()
---
+++ js/services/product-services.js
@@ -0,0 +1,62 @@
+const BASE_URL = "http://localhost:3000/products";
+
+const productList = () => {
+  return fetch(`${BASE_URL}`)
+    .then(res => res.json())
+    .catch(err => console.log(err))
+}
+
+// const productList = async () => {
+//   try {
+//     const res = await fetch(`${BASE_URL}`)  
+//     return await res.json()
+//   } catch (err) {
+//     return console.log(err)  
+//   }
+// }
+
+const createProduct = (name, price, image) => {
+  return fetch(`${BASE_URL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, price, image })
+  })
+    .then(res => res.json())
+    .catch(err => console.log(err))
+}
+
+const deleteProduct = (id) => {
+  return fetch("http://localhost:3000/products/"+`${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    }
+    // body: JSON.stringify({name, price, image})
+  })
+    .then(res => res.json())
+    .catch(err => console.log(err))
+}
+// const deleteProduct = async (id) => {
+//   debugger
+//   try {
+//     const conexion = await fetch(`${BASE_URL}/${id}`, {
+//       method: "DELETE",
+//       headers: { "Content-type": "application/json" },
+//     });
+
+//     const conexionConvertida = conexion.json();
+
+//     return conexionConvertida;
+
+//   } catch (error) {
+//     console.error(`Error al eliminar el producto: ${id}`);
+//   }
+// }
+
+export const servicesProducts = {
+  productList, createProduct, deleteProduct
+}
+
+
